4fa682d2a3b331b12ff48d619681f6640d85d3ee
latimercaleb <nohitokaze@gmail.com>
2018-06-21 00:55:45 -0400

    Revert "Unwrapping"
    This reverts commit 60fe33c689931bb250585c19daeb3072e20192ca.

---
+++ practice-app/src/card.js
@@ -0,0 +1,32 @@
+/*
+  Card component
+  Reads data fetched from  parent PropX
+  Produces Cards with sylized data
+*/
+import React, {Component} from 'react'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+const listStyles = {
+      border: '1px groove black',
+      'background-color': '#E2F1FF',
+      'border-radius': '5px',
+      'margin': '3px'
+}
+class Card extends Component{
+  render(){
+    return(
+          <List style={listStyles} dense>
+            {this.props.data.map(dataItem => {
+                  return (
+                        <ListItem key={dataItem.id}>
+                              <ListItemText primary={dataItem.name}
+                                                    secondary={dataItem.company.name}/>
+                        </ListItem>
+                  )
+            })}
+          </List>
+    )
+  }
+}
+export default Card
